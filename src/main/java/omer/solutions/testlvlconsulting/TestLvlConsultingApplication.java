8b6e0d62225adf393dc0da0d4516455d91d92566
Omer <rafaelxdale@gmail.com>
2024-08-22 17:08:35 -0500

    Initial commit

---
+++ src/main/java/omer/solutions/testlvlconsulting/TestLvlConsultingApplication.java
@@ -0,0 +1,13 @@
+package omer.solutions.testlvlconsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLvlConsultingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLvlConsultingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/omer/solutions/testlvlconsulting/TestLvlConsultingApplicationTests.java
@@ -0,0 +1,13 @@
+package omer.solutions.testlvlconsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLvlConsultingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
