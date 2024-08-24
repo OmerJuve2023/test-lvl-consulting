5b119275338da67dd6c29da1e1a0921af43b71bd
Omer <rafaelxdale@gmail.com>
2024-08-23 20:22:44 -0500

    add AuthenticationEntryPointWithError and JwtUtils.java to jwt token

---
+++ src/main/java/omer/solutions/testlvlconsulting/config/JwtUtils.java
@@ -0,0 +1,69 @@
+package omer.solutions.testlvlconsulting.config;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import omer.solutions.testlvlconsulting.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtils {
+    @Value("${jwt.jwtCookieName}")
+    private static final String jwtCookie = "access_token";
+
+    @Value("${jwt.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${jwt.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String getJwtFromCookies(HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, jwtCookie);
+        if (cookie != null) {
+            return cookie.getValue();
+        } else {
+            return null;
+        }
+    }
+
+    public ResponseCookie generateJwtCookie(User user) {
+        String jwt = generateTokenFromUsername(user.getUsername());
+        return ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).build();
+    }
+
+    public ResponseCookie getCleanJwtCookie() {
+        return ResponseCookie.from(jwtCookie, null).path("/api").build();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
+        return false;
+    }
+
+    public String generateTokenFromUsername(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+}
