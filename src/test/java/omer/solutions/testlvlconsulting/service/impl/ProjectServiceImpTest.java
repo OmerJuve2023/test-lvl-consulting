117faa7f5684d2d0ceb10ad5f09c1cf882b1b28d
Omer <rafaelxdale@gmail.com>
2024-08-26 12:27:33 -0500

    add profiles to project

--- src/test/java/omer/solutions/testlvlconsulting/service/impl/ProjectServiceImpTest.java
+++ src/test/java/omer/solutions/testlvlconsulting/service/impl/ProjectServiceImpTest.java
@@ -2,20 +2,26 @@ package omer.solutions.testlvlconsulting.service.impl;
 
 import omer.solutions.testlvlconsulting.dto.response.ProjectReponse;
 import omer.solutions.testlvlconsulting.entity.Project;
+import omer.solutions.testlvlconsulting.entity.User;
 import omer.solutions.testlvlconsulting.repository.ProjectRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectServiceImpTest {
 
     @Mock
@@ -23,6 +29,8 @@ class ProjectServiceImpTest {
 
     @InjectMocks
     private ProjectServiceImp projectService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
@@ -31,6 +39,10 @@ class ProjectServiceImpTest {
 
     @Test
     void getProjectById() {
+        User user = new User();
+        user.setId(1L);
+        user.setPassword(passwordEncoder.encode("password"));
+        user.setUsername("user1");
 
         Project project = new Project();
         project.setId(1L);
@@ -39,7 +51,7 @@ class ProjectServiceImpTest {
         project.setEstado("Active");
         project.setFechaInicio(new Date());
         project.setFechaFin(new Date());
-        project.setUser(null);
+        project.setUser(user);
 
         when(projectRepository.findByIdAndUser(1L, 1L)).thenReturn(project);
 
